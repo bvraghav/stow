b246b2396d96a7daed8dce0a9dd48778320b1b79
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2024-09-08 01:26:31 +0530

    Place guard against failure to ‘pkill emacs.’

--- user-home/.functions.zsh
+++ user-home/.functions.zsh
@@ -8,6 +8,6 @@ refire_emacs () {
   local REPLY
   read -q "REPLY?This will kill all user emacs sessions. Continue [Y/n]? " && \
     echo && \
-    pkill -9 emacs && \
+    if pgrep emacs ; then pkill -9 emacs; fi && \
     fire_emacs -nf -wd
 }
