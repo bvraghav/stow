6caf7eae4baf03d32c31e74d4a33ed18bcc18045
Raghav B. Venkataramaiyer <bvraghav@gmail.com>
2025-09-06 07:22:54 +0530

    Explicate default response in ‘refire_emacs.’

--- user-home/.functions.zsh
+++ user-home/.functions.zsh
@@ -6,7 +6,7 @@
 
 refire_emacs () {
   local REPLY
-  read -q "REPLY?This will kill all user emacs sessions. Continue [Y/n]? " && \
+  read -q "REPLY?This will kill all user emacs sessions. Continue (y|n) [N]? " && \
     echo && \
     if pgrep emacs ; then pkill -9 emacs; fi && \
     fire_emacs -nf -wd
